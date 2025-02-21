ac598879545a9515890aea94524b15882e6e2846
Vijay Bhatia <vbhatia991@gmail.com>
2025-02-21 15:04:15 -0500

    update existing programs

--- Easy/kth_largest.py
+++ Easy/kth_largest.py
@@ -1,11 +1,74 @@
 """
-Problem: Kth Largest Element in an Array
+215. Kth Largest Element in an Array 
+
 Find the k thlargest element in an unsorted array.
-Note that it is the k th
-  largest element in sorted order, not the k th
-  distinct element
-nums = [3, 2, 1, 5, 6, 4], k = 2
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
 """
+
+#Sol 1
+def kth_largest(nums,k): 
+    nums.sort() 
+    return nums[-k]
+#Sol 2
+def kth_largest(nums,k): 
+    import heapq 
+    return heapq.nlargest(k,nums)[-1]
+#Sol 3
+def kth_largest(nums,k): 
+    import heapq 
+    heapq.heapify(nums) 
+    return heapq.nlargest(k,nums)[-1]
+
+
+#Sol 4 
+# In this approach, we use a min-heap to keep track of the k largest elements in the array.
+# We initialize a min-heap with the first k elements of nums.
+
+# heapq.heapify(min_heap) transforms the first k elements of nums into a min-heap.
+# The for loop iterates over the remaining elements in nums.
+# If an element is larger than the smallest element in the heap (min_heap[0]), it replaces the smallest element to maintain the k largest elements in the heap.
+# Finally, min_heap[0] returns the kth largest element.
+
+
+import heapq
+
+def kth_largest(nums, k):
+    min_heap = nums[:k]
+    heapq.heapify(min_heap)
+    for num in nums[k:]:
+        if num > min_heap[0]:
+            heapq.heappop(min_heap)
+            heapq.heappush(min_heap, num)
+    return min_heap[0]
+
+if __name__ == "__main__":
+    nums = [3,2,1,5,6,4]
+    k = 2
+    print(kth_largest(nums,k))
+    nums = [3,2,3,1,2,4,5,5,6]
+    k = 4
+    print(kth_largest(nums,k))
+    nums = [3,2,1,5,6,4]
+    k = 2
+    print(kth_largest(nums,k))
+    nums = [3,2,3,1,2,4,5,5,6]
+    k = 4 
+
+
+
+#Sol 5
 import heapq
 
 class findk():
