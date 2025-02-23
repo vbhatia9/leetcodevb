7c38178f58ea2df1b5066d62f1d202665b3bc189
Vijay Bhatia <vbhatia991@gmail.com>
2025-02-23 12:34:00 -0500

    add new sql prgram second highest sal

---
+++ SQL/Easy/second_highest_sal.sql
@@ -0,0 +1,71 @@
+/*
+Write a SQL query to get the second highest salary from the Employee table.
+176. Second Highest Salary
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+*/
+--Sol 1 Autopilot
+WITH EmployeeSalary AS (
+  SELECT DISTINCT salary
+  FROM Employee
+  ORDER BY salary DESC
+  LIMIT 1 OFFSET 1
+)
+SELECT 
+  (SELECT salary FROM EmployeeSalary) AS SecondHighestSalary;
+--Sol 2 web
+--someone gave this solution
+SELECT 
+    (SELECT DISTINCT salary
+     FROM Employee
+     ORDER BY salary DESC
+     LIMIT 1 OFFSET 1) AS SecondHighestSalary; 
+    
