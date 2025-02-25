ad7927a48aaada8e81764ee1ec88458f9e87ca04
Vijay Bhatia <vbhatia991@gmail.com>
2025-02-24 20:17:52 -0500

    add valid palindrome

---
+++ Easy/valid_palindrome.py
@@ -0,0 +1,54 @@
+"""
+125. Valid Palindrome
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+"""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #Sol 1
+        # s = ''.join(filter(str.isalnum, s)).lower()
+        # return s == s[::-1]
+       
+    
+   
+        #Sol 2
+        newstr = ""
+        for c in s:
+            if  c.isalnum():
+               newstr += c.lower() 
+        print(newstr)
+        return newstr == newstr[::-1]
+        
+        
+    
+if __name__ == "__main__":
+    s = "A man, a plan, a canal: Panama"
+    obj1 = Solution()
+    result = obj1.isPalindrome(s)   
+    print(result)
