c16d406186f93cc3bae246433b38f15e9d457539
Vijay Bhatia <vbhatia991@gmail.com>
2025-03-11 23:06:13 -0400

    longest

---
+++ Medium/longest.py
@@ -0,0 +1,15 @@
+"""
+Longest Consecutive Sequence
+Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. 
+The elements do not have to be consecutive in the original array.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+
+Input: nums = [2,20,4,10,3,4,5]
+
+Output: 4
+"""
