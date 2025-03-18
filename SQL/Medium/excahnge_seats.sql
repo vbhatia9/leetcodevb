27aaa72c63bd7854589b262dca49ca7bd4dab42a
Vijay Bhatia <vbhatia991@gmail.com>
2025-03-17 23:59:06 -0400

    626. Exchange Seats

--- SQL/Medium/excahnge_seats.sql
+++ SQL/Medium/excahnge_seats.sql
@@ -1,4 +1,19 @@
 /*626. Exchange Seats
+Table: Seat
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+The ID sequence always starts from 1 and increments continuously.
+ 
+
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
 
 */
