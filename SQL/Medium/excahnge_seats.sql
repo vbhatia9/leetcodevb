d27f82e42c47bbb6f2f600fb4ad4489aff22e32c
Vijay Bhatia <vbhatia991@gmail.com>
2025-03-18 00:01:19 -0400

    626. Exchange Seats

--- SQL/Medium/excahnge_seats.sql
+++ SQL/Medium/excahnge_seats.sql
@@ -12,8 +12,18 @@ Each row of this table indicates the name and the ID of a student.
 The ID sequence always starts from 1 and increments continuously.
  
 
-Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, 
+the id of the last student is not swapped.
 
 Return the result table ordered by id in ascending order.
 
 */
+-- not sure 
+SELECT id, 
+       CASE 
+           WHEN MOD(id, 2) = 0 THEN LAG(student, 1) OVER (ORDER BY id)
+           WHEN MOD(id, 2) = 1 THEN LEAD(student, 1) OVER (ORDER BY id)
+           ELSE student 
+       END AS student
+FROM Seat
+ORDER BY id;
