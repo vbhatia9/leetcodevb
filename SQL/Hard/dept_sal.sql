57033918e14d450d2ca97d6228e4f1b4cf03a369
Vijay Bhatia <vbhatia991@gmail.com>
2025-03-20 12:54:49 -0400

    update dept_sql

--- SQL/Hard/dept_sal.sql
+++ SQL/Hard/dept_sal.sql
@@ -1,23 +1,34 @@
 /* SQL/Hard/
- Department Top Three Salaries (Hard)
+185. Department Top Three Salaries
+https://leetcode.com/problems/department-top-three-salaries/
 
 Problem: Find the top 3 highest salaries in each department.
 
+
 */
 
+-- wrong answer
+-- WITH RankedSalaries AS (
+--     SELECT 
+--         DepartmentId,
+--         EmployeeId,
+--         Salary,
+--         RANK() OVER (PARTITION BY DepartmentId ORDER BY Salary DESC) AS Rank -- Rank salaries within each department
+--     FROM Employee
+-- )
+-- SELECT 
+--     DepartmentId,
+--     EmployeeId,
+--     Salary
+-- FROM RankedSalaries
+-- WHERE Rank <= 3
+-- ORDER BY DepartmentId, Rank;
 
-WITH RankedSalaries AS (
-    SELECT 
-        DepartmentId,
-        EmployeeId,
-        Salary,
-        RANK() OVER (PARTITION BY DepartmentId ORDER BY Salary DESC) AS Rank
-    FROM Employee
-)
-SELECT 
-    DepartmentId,
-    EmployeeId,
-    Salary
-FROM RankedSalaries
-WHERE Rank <= 3
-ORDER BY DepartmentId, Rank;
+SELECT department, employee, salary 
+FROM (
+    SELECT d.name AS department, e.name AS employee, e.salary,
+           DENSE_RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+) temp
+WHERE rnk <= 3;
