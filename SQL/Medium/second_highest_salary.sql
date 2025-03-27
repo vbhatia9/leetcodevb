3a726979a105c87eabcac946da75e48322502885
Vijay Bhatia <vbhatia991@gmail.com>
2025-03-26 22:14:58 -0400

    update sql

--- SQL/Medium/second_highest_salary.sql
+++ SQL/Medium/second_highest_salary.sql
@@ -53,10 +53,11 @@ Output:
 +---------------------+
 */
 
-SELECT 
-    (SELECT DISTINCT salary
+SELECT (
+    SELECT DISTINCT salary
      FROM Employee
      ORDER BY salary DESC
-     LIMIT 1 OFFSET 1) AS SecondHighestSalary;  -- OFFSET 1 skips the highest salary and retrieves the second highest salary
+     LIMIT 1 OFFSET 1
+     ) AS SecondHighestSalary;  -- OFFSET 1 skips the highest salary and retrieves the second highest salary
 -- The provided SQL query is designed to retrieve the second highest distinct salary from the Employee table. This query is executed using a subquery within the SELECT statement to retrieve the salary value that is distinct and second highest. The subquery is ordered by salary in descending order and limited to retrieve the second highest salary using the OFFSET 1 clause. The result is returned in the specified format.      
-```
+``` 
