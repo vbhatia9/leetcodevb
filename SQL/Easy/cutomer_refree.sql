6eeb287541eadde05e33555f2177430a0d804c9b
Vijay Bhatia <vbhatia991@gmail.com>
2025-04-05 10:26:26 -0400

    adding EASY SQL cutomer reference

--- SQL/Easy/cutomer_refree.sql
+++ SQL/Easy/cutomer_refree.sql
@@ -44,4 +44,8 @@ Output:
 | Zack |
 +------+
 */
+--solution 1
+SELECT name FROM customer WHERE referee_id <> 2 OR referee_id IS NULL;
 
+--solution 2
+SELECT name FROM customer WHERE referee_id != 2 OR referee_id IS NULL;
