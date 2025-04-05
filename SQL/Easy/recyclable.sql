cbefa193c77042df7dda6f210ba4daa0222476d1
Vijay Bhatia <vbhatia991@gmail.com>
2025-04-05 10:19:34 -0400

    adding EASY SQL recyclable

--- SQL/Easy/recyclable.sql
+++ SQL/Easy/recyclable.sql
@@ -14,4 +14,31 @@ recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this produc
  
 
 Write a solution to find the ids of products that are both low fat and recyclable.
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Only products 1 and 3 are both low fat and recyclable.
 """
