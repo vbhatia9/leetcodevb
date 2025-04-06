787ecf4657250078f56201c28be47e089461c937
Vijay Bhatia <vbhatia991@gmail.com>
2025-04-06 14:38:05 -0400

    adding EASY SQL article views

--- SQL/Easy/article_views.sql
+++ SQL/Easy/article_views.sql
@@ -16,6 +16,9 @@ Note that equal author_id and viewer_id indicate the same person.
  
 
 Write a solution to find all the authors that viewed at least one of their own articles.
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
 */
 select distinct author_id  as id
 from Views
