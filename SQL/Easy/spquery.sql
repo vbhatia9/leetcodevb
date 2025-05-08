8779359fefd89068951e30451fceddc4eb9f3211
Vijay Bhatia <vbhatia991@gmail.com>
2025-05-08 07:00:15 -0400

    new query

---
+++ SQL/Easy/spquery.sql
@@ -0,0 +1,33 @@
+ /*
+Employee table with three columns emp
+ 
+ID, Salary, DeptID
+ 
+10, 1000, 2
+ 
+20, 5000, 3
+ 
+30, 3000, 2
+ 
+Department table with two columns: dept
+ 
+ID, DeptName
+ 
+1, Marketing
+ 
+2, IT
+ 
+3, Finance
+
+Emp Address  emp_addrs
+
+id address
+
+10  India
+
+20  UK
+
+30  US
+ 
+find  salary for each employee along with deptname and emp address*/
+
